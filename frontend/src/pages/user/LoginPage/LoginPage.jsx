0ea2cabac9e5c6a72ad83b181f79fc2d5e0ee829
Faraz shafi <farazpachu777@gmail.com>
2024-09-15 15:31:22 +0530

    created signIn page with responive

--- frontend/src/pages/user/LoginPage/LoginPage.jsx
+++ frontend/src/pages/user/LoginPage/LoginPage.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 import "./LoginPage.css";
-import Header from "../../../components/Header/Header"
-
+import Header from "../../../components/Header/Header";
+import { Box, Button, Center, Image, Text } from "@chakra-ui/react";
+import headphoneImg from "../../../assets/images/walpaper/headphone.jpeg";
 
 const LoginPage = () => {
   return (
     <>
       <Header />
+      <Box
+        className="signIn-main-div"
+        mt={{ base: "50px", md: "30px", lg: "50px" }}
+      >
+        <Text
+          fontSize={{ base: "30px", md: "20", lg: "30px" }}
+          className="signIn-heading"
+        >
+          SignIn
+        </Text>
+        <Center>
+          <Box padding={{lg:"5% 3% 5% 3%",md:"5% 3% 5% 3%", base:"5% 10%"}} className="content-wrapper">
+            <Box
+              display={{base:"none",lg:"inline",md:'inline'}}
+              width={{lg:"50%",md:"50%",base:"80%"}}
+            >
+              <Image borderRadius={"20px"} width={{lg:"260px"}} src={headphoneImg} alt="sign-in" />
+            </Box>
+            <Box
+              width={{lg:"50%",md:"50%",base:"100%"}}
+            >
+              <form>
+                <input style={{marginBottom:"20px"}} className="form-input" placeholder="Enter your gmail" type="text" name="gmail"/><br />
+                <input className="form-input" placeholder="Enter your password" type="text" name="passwrod"/>
+                <Text mt={"10px"} fontSize={{base:"10px",md:"20px",lg:"15px"}} className="forgot-password">forgot password?</Text>
+                <Button color={"white"} bg={"#FF7F11"} mt={"30px"} width={"100%"}>SignIn</Button>
+                <Text fontSize={{base:"10px",md:"20px",lg:"15px"}} mt={"10px"}><a href="/register">Create an Account</a></Text>
+              </form>
+            </Box>
+          </Box>
+        </Center>
+      </Box>
     </>
   );
 };
