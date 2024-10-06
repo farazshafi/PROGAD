87a883ed38daae6076b4f9ee83bd348034555ebe
Faraz shafi <farazpachu777@gmail.com>
2024-10-06 21:45:40 +0530

    created cart page without responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,33 +2,33 @@ import LoginPage from "./pages/user/LoginPage/LoginPage";
 import OtpPage from "./pages/user/OtpPage/OtpPage";
 import RegisterPage from "./pages/user/RegisterPage/RegisterPage";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import routing components
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import routing components
 import NotFoundPage from "./pages/user/NotFoundPage/NotFound";
-import HomePage from "./pages/user/HomePage/HomePage"
+import HomePage from "./pages/user/HomePage/HomePage";
 import NewArrivalsPage from "./pages/user/NewArrivalsPage/NewArrivalsPage";
 import TopProducts from "./pages/user/TopProducts/TopProducts";
 import OfferPage from "./pages/user/OfferPage/OfferPage";
 import ProductsPage from "./pages/user/ProductsPage/ProductsPage";
-import ProductCard from "./components/ProductCard/ProductCard";
 import ProductDetailsPage from "./pages/user/ProductDetailsPage/ProductDetailsPage";
-
+import CartPage from "./pages/user/CartPage/CartPage";
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/otp" element={<OtpPage />} />
-          <Route path="/newarrivals" element={<NewArrivalsPage />} />
-          <Route path="/topproducts" element={<TopProducts />} /> 
-          <Route path="/offerpage" element={<OfferPage />} /> 
-          <Route path="/products" element={<ProductsPage />} /> 
-          <Route path="/product_details/:id" element={<ProductDetailsPage />} /> 
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/otp" element={<OtpPage />} />
+        <Route path="/newarrivals" element={<NewArrivalsPage />} />
+        <Route path="/topproducts" element={<TopProducts />} />
+        <Route path="/offer" element={<OfferPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/product_details/:id" element={<ProductDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/OurButton/OurButton.jsx
+++ frontend/src/components/OurButton/OurButton.jsx
@@ -1,7 +1,7 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 
-const OurButton = ({ text, type = "button", handleClick, page }) => {
+const OurButton = ({ text, type = "button", handleClick, page, w }) => {
   return (
     <ChakraProvider>
       <Button
@@ -9,7 +9,7 @@ const OurButton = ({ text, type = "button", handleClick, page }) => {
         color="white"
         type={type}
         bg="#FF7F11"
-        width={page === "otp" || page === "product_details" ? "100%" : "auto"}
+        width={page === "otp" || page === "product_details" || w === "100"? "100%" : "auto"}
         transition="background-color 0.5s ease, color 0.5s ease"
         _hover={{
           bg: "#262626",
